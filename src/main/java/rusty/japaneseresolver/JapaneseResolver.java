ccf492197bcac73b323dc7ad42ad286b1f8694a2
RusTy <mathiasp9999@gmail.com>
2021-12-09 02:26:00 +0100

    very basic functionality implemented. look-up is blocks UI.

--- src/main/java/rusty/japaneseresolver/JapaneseResolver.java
+++ src/main/java/rusty/japaneseresolver/JapaneseResolver.java
@@ -15,7 +15,11 @@ public class JapaneseResolver implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+	}
+
+	private void resolve() {
+		
 	}
 }
